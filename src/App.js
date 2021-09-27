7f061058c8f89d42eb8a9bb181b4ff90522152d4
Daniel Won <dan.j.won@gmail.com>
2021-09-27 06:14:28 -0500

    add task submit function

--- src/App.js
+++ src/App.js
@@ -25,7 +25,14 @@ function App() {
 		},
 	]);
 
-	//Delete Task
+	// Add task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1;
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
+	};
+
+	// Delete Task
 	const deleteTask = (id) => {
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
@@ -42,7 +49,7 @@ function App() {
 	return (
 		<div className='container'>
 			<Header />
-			<AddTask />
+			<AddTask onAdd={addTask} />
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
